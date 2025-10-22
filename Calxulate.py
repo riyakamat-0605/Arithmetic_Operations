37f082ad7fce4940ba9c665c7816a6a7d95d955c
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-22 11:36:17 +0530

    Save to local

--- Calxulate.py
+++ Calxulate.py
@@ -4,8 +4,18 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a / b
+
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 
 print(f"Addition: {add(a, b)}")
 print(f"Subtraction: {subtract(a, b)}")
+print(f"Multiplication: {multiply(a, b)}")
+print(f"Division: {divide(a, b)}")
