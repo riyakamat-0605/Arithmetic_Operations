bc7a4c1e4e2cbcf33015a0769d9c326ebeb07cde
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-22 11:31:20 +0530

    Create Calxulate.py

---
+++ Calxulate.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print(f"Addition: {add(a, b)}")
+print(f"Subtraction: {subtract(a, b)}")
